acd036522796a97cbcf468448f461c71edbeb5bb
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-10-12 20:04:12 +0800

    implemented onCommentDeleteLogic

--- functions/src/business-logics/comment-logics.ts
+++ functions/src/business-logics/comment-logics.ts
@@ -1,3 +1,5 @@
+import {Entity} from "../db-structure";
+import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
 // TODO write onCommentCreateLogic
 //  increment post commentsCount
 //  create a notification for post's author
@@ -7,5 +9,39 @@
 //  update the notification for post's author
 
 // TODO write onCommentDeleteLogic
+export const onCommentDeleteLogic: LogicConfig = {
+  name: "onCommentDeleteLogic",
+  actionTypes: ["delete"],
+  modifiedFields: "all",
+  entities: [Entity.Comment],
+  logicFn: async (action) => {
+    const {
+      eventContext: {
+        docPath,
+        docId,
+        uid,
+      },
+    } = action;
+
+    const commentResultDoc : LogicResultDoc = {
+      "action": "delete",
+      "dstPath": docPath,
+    };
+    const notificationResultDoc : LogicResultDoc = {
+      "action": "delete",
+      "dstPath": `users/${uid}/notifications/${docId}`,
+    };
+
+    const logicResultDocs : LogicResultDoc[] = [];
+    logicResultDocs.push(commentResultDoc);
+    logicResultDocs.push(notificationResultDoc);
+
+    return {
+      name: "onCommentDeleteLogic",
+      status: "finished",
+      documents: logicResultDocs,
+    };
+  },
+};
 //  decrement post commentsCount
 //  delete the notification for post's author
