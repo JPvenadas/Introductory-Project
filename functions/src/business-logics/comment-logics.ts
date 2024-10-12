e6806bd44547bb624c5c05bf09d9e1f50f0d79d1
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-10-12 08:35:26 +0800

    implemented onCommentCreateLogic

--- functions/src/business-logics/comment-logics.ts
+++ functions/src/business-logics/comment-logics.ts
@@ -1,7 +1,71 @@
 // TODO write onCommentCreateLogic
+import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
+import {Entity} from "../db-structure";
+import {firestore} from "firebase-admin";
+import DocumentData = firestore.DocumentData;
+import {createUserView} from "./utils";
+import {UserView} from "../types";
+import {admin} from "emberflow/lib";
+
+export const onCommentCreateLogic: LogicConfig = {
 //  increment post commentsCount
 //  create a notification for post's author
+  name: "onCommentCreateLogic",
+  actionTypes: ["create"],
+  modifiedFields: "all",
+  entities: [Entity.Comment],
+  logicFn: async (action) => {
+    const {
+      eventContext: {
+        docId,
+        docPath,
+      },
+      user,
+      modifiedFields,
+    } = action;
+    const {...fields} = modifiedFields;
+    const now: firestore.Timestamp = admin.firestore.Timestamp.now();
 
+    const userView: UserView = createUserView(user);
+
+
+    const commentDoc: DocumentData ={
+      ...fields,
+      "@id": docId,
+      "createdBy": userView,
+      "createdAt": now,
+      "repliesCount": 0,
+    };
+    const commentLogicResultDoc: LogicResultDoc = {
+      "action": "create",
+      "dstPath": docPath,
+      "doc": commentDoc,
+    };
+    const notificationDoc: DocumentData = {
+      "@id": docId,
+      "createdBy": userView,
+      "createdAt": now,
+      "type": "comment",
+      "read": false,
+    };
+
+    const notificationLogicResultDoc: LogicResultDoc = {
+      "action": "create",
+      "dstPath": `users/${fields.postId}/notifications/${docId}`,
+      "doc": notificationDoc,
+    };
+
+    const logicResultDocs: LogicResultDoc[] = [];
+    logicResultDocs.push(commentLogicResultDoc);
+    logicResultDocs.push(notificationLogicResultDoc);
+
+    return {
+      name: "onCommentCreateLogic",
+      status: "finished",
+      documents: logicResultDocs,
+    };
+  },
+};
 // TODO write onCommentUpdateLogic
 //  allow user to update comment content
 //  update the notification for post's author
