8995a1aa5c3855f6f733520d19fc8ca4b67f0e83
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-10-12 19:46:33 +0800

    partially finished onCommentUpdateLogic

--- functions/src/business-logics/comment-logics.ts
+++ functions/src/business-logics/comment-logics.ts
@@ -1,8 +1,49 @@
+import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
+import {Entity} from "../db-structure";
+import {firestore} from "firebase-admin";
+import DocumentData = firestore.DocumentData;
 // TODO write onCommentCreateLogic
 //  increment post commentsCount
 //  create a notification for post's author
 
 // TODO write onCommentUpdateLogic
+
+
+export const onCommentUpdateLogic: LogicConfig = {
+  name: "onCommentUpdateLogic",
+  actionTypes: ["update"],
+  modifiedFields: "all",
+  entities: [Entity.Comment],
+  logicFn: async (action) => {
+    const {
+      eventContext: {
+        docPath,
+      },
+      modifiedFields,
+    } = action;
+
+    const {...fields} = modifiedFields;
+    const commentDoc: DocumentData = {...fields};
+
+    const notificationDoc: DocumentData = {
+
+    };
+
+    const commentLogicResultDoc: LogicResultDoc = {
+      action: "merge",
+      dstPath: docPath,
+      doc: commentDoc,
+    };
+    const logicResultDocs: LogicResultDoc[] = [];
+    logicResultDocs.push(commentLogicResultDoc);
+
+    return {
+      name: "onCommentUpdateLogic",
+      status: "finished",
+      documents: logicResultDocs,
+    };
+  },
+};
 //  allow user to update comment content
 //  update the notification for post's author
 
