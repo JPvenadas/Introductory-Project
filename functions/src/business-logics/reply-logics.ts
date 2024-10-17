83a81b5be3d432d2820e23aefed363caa74aed82
Noel Emaas <nemaas@gbox.adnu.edu.ph>
2024-10-17 22:24:13 +0800

    implemented create reply business logic

--- functions/src/business-logics/reply-logics.ts
+++ functions/src/business-logics/reply-logics.ts
@@ -1,6 +1,109 @@
-// TODO write onReplyCreateLogic
-//  increment comment repliesCount
-//  create a notification for comment's author
+import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
+import {Comment, Post, UserView} from "../types";
+import {createUserView} from "./utils";
+import {admin, db} from "emberflow/lib";
+import {DocumentData} from "firebase-admin/firestore";
+import {firestore} from "firebase-admin";
+
+export const onReplyCreateLogic: LogicConfig = {
+  name: "onReplyCreateLogic",
+  actionTypes: ["create"],
+  modifiedFields: "all",
+  entities: ["reply"],
+  logicFn: async (action) => {
+    const {
+      eventContext: {
+        docPath,
+        docId,
+      },
+      user,
+      modifiedFields,
+    } = action;
+
+    const replyRef = db.doc(docPath);
+    const commentDocRef = replyRef.parent.parent;
+
+    if (!commentDocRef) {
+      return {
+        name: "onReplyCreateLogic",
+        status: "error",
+        documents: [],
+        message: `Invalid comment docpath at ${docPath}`,
+      };
+    }
+
+    const commentRef = db.doc(commentDocRef.path);
+    const commentDocSnapshot = await commentRef.get();
+    const commentDoc = commentDocSnapshot.data() as Comment;
+
+    if (!commentDoc) {
+      return {
+        name: "onReplyCreateLogic",
+        status: "error",
+        documents: [],
+        message: `Comment ${commentDocRef.id} does not exist`,
+      };
+    }
+
+    const commentLogicResultDoc: LogicResultDoc = {
+      "action": "merge",
+      "dstPath": commentRef.path,
+      "instructions": {
+        "repliesCount": "++",
+      },
+    };
+
+    const userView: UserView = createUserView(user);
+    const now: firestore.Timestamp = admin.firestore.Timestamp.now();
+
+    const replyDoc: DocumentData = {
+      ...modifiedFields,
+      "@id": docId,
+      "createdBy": userView,
+      "createdAt": now.toDate(),
+    };
+
+    const replyLogicResultDoc: LogicResultDoc = {
+      "action": "create",
+      "dstPath": docPath,
+      "doc": replyDoc,
+    };
+
+    const {
+      post,
+      createdBy: {
+        "@id": commentAuthorId,
+      },
+    } = commentDoc;
+
+    const notificationDoc: DocumentData = {
+      "@id": docId,
+      "createdBy": userView,
+      "createdAt": now.toDate(),
+      "type": "reply",
+      "read": false,
+      "post": post as Post,
+    };
+
+    const notifDocPath = `users/${commentAuthorId}/notifications/${docId}`;
+    const notificationLogicResultDoc: LogicResultDoc = {
+      "action": "create",
+      "dstPath": notifDocPath,
+      "doc": notificationDoc,
+    };
+
+    const logicResultDocs: LogicResultDoc[] = [];
+    logicResultDocs.push(replyLogicResultDoc);
+    logicResultDocs.push(commentLogicResultDoc);
+    logicResultDocs.push(notificationLogicResultDoc);
+
+    return {
+      name: "onReplyCreateLogic",
+      status: "finished",
+      documents: logicResultDocs,
+    };
+  },
+};
 
 // TODO write onReplyUpdateLogic
 //  allow user to update reply content
