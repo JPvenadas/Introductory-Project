6b5b58a19ca80d4df6d81c4426b07ae3860f8f3f
Noel Emaas <nemaas@gbox.adnu.edu.ph>
2024-10-17 21:49:00 +0800

    implemented comment update business logic

--- functions/src/business-logics/comment-logics.ts
+++ functions/src/business-logics/comment-logics.ts
@@ -1,10 +1,46 @@
+import {Entity} from "emberflow/lib";
+import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
+import {DocumentData} from "firebase-admin/firestore";
+
 // TODO write onCommentCreateLogic
 //  increment post commentsCount
 //  create a notification for post's author
 
-// TODO write onCommentUpdateLogic
-//  allow user to update comment content
-//  update the notification for post's author
+export const onCommentUpdateLogic: LogicConfig = {
+  name: "onCommentUpdateLogic",
+  actionTypes: ["update"],
+  modifiedFields: ["all"],
+  entities: [Entity.Comment],
+  logicFn: async (action) => {
+    const {
+      eventContext: {
+        docPath,
+      },
+      modifiedFields: {
+        content,
+      },
+    } = action;
+
+    const commentDoc: DocumentData = {
+      content,
+    };
+
+    const commentLogicResultDoc: LogicResultDoc = {
+      "action": "merge",
+      "dstPath": docPath,
+      "doc": commentDoc,
+    };
+
+    const logicResultDocs: LogicResultDoc[] = [];
+    logicResultDocs.push(commentLogicResultDoc);
+
+    return {
+      name: "onCommentUpdateLogic",
+      status: "finished",
+      documents: logicResultDocs,
+    };
+  },
+};
 
 // TODO write onCommentDeleteLogic
 //  decrement post commentsCount
