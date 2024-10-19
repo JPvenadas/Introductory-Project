ec47bd5658c4aea87e9b08b6b47ea98892194ab9
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-10-19 21:09:15 +0800

    implement onReplyUpdateLogic

--- functions/src/business-logics/reply-logics.ts
+++ functions/src/business-logics/reply-logics.ts
@@ -5,6 +5,46 @@
 // TODO write onReplyUpdateLogic
 //  allow user to update reply content
 
+import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
+import {Entity} from "../db-structure";
+import {firestore} from "firebase-admin";
+import DocumentData = firestore.DocumentData;
+
+export const onReplyUpdateLogic: LogicConfig = {
+  name: "onReplyUpdateLogic",
+  actionTypes: ["update"],
+  modifiedFields: "all",
+  entities: [Entity.Reply],
+  logicFn: async (action) => {
+    const {
+      eventContext: {
+        docPath,
+      },
+      modifiedFields,
+    } = action;
+
+    const replyDoc: DocumentData = {
+      ...modifiedFields,
+    };
+
+    const replyLogicResultDoc: LogicResultDoc = {
+      "action": "merge",
+      "dstPath": docPath,
+      "doc": replyDoc,
+    };
+
+    const logicResultDocs: LogicResultDoc[] = [];
+    logicResultDocs.push(replyLogicResultDoc);
+
+    return {
+      "name": "onReplyUpdateLogic",
+      "status": "finished",
+      "documents": logicResultDocs,
+
+    };
+  },
+};
+
 // TODO write onReplyDeleteLogic
 //  decrement comment repliesCount
 //  delete the notification for comment's author
