7152085af8ad93f6453ab3be0105de5e652b7706
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-11-02 09:11:52 +0800

    implemented onUnlikeLogic and unit tests

--- functions/src/business-logics/like-logics.ts
+++ functions/src/business-logics/like-logics.ts
@@ -1,7 +1,89 @@
+import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
+import {Entity} from "../db-structure";
+import {db} from "emberflow/lib";
 // TODO write onLikeLogic
 //  increment post likesCount
 //  add user's userView to post's likes collection
 //  create a notification for post's author
+
+
+export const onUnlikeLogic: LogicConfig = {
+  name: "onUnlikeLogic",
+  actionTypes: ["delete"],
+  modifiedFields: "all",
+  entities: [Entity.Like],
+  logicFn: async (action) => {
+    const {
+      eventContext: {
+        docPath,
+        docId,
+      },
+    } = action;
+
+    const likeRef = db.doc(docPath);
+    const postRef = likeRef.parent.parent;
+
+    if (!postRef) {
+      return {
+        name: "onUnlikeLogic",
+        status: "error",
+        error: {
+          message: `Invalid like document path at ${docPath}`,
+        },
+        documents: [],
+      };
+    }
+
+    const postSnapshot = await postRef.get();
+    const postDoc = postSnapshot.data();
+    if (postDoc === undefined) {
+      return {
+        name: "onUnlikeLogic",
+        status: "error",
+        error: {
+          message: `Post ${postRef.id} does not exist`,
+        },
+        documents: [],
+      };
+    }
+
+    const {
+      "@id": postId,
+      createdBy: {
+        "@id": postAuthorId,
+      },
+    } = postDoc;
+
+    const logicResultDocs: LogicResultDoc[] = [];
+    const postLogicResultDoc: LogicResultDoc = {
+      action: "merge",
+      dstPath: `posts/${postId}`,
+      instructions: {
+        likesCount: "--",
+      },
+    };
+    const likeLogicResultDoc: LogicResultDoc = {
+      action: "delete",
+      dstPath: docPath,
+    };
+
+    const notificationLogicResultDoc: LogicResultDoc = {
+      action: "delete",
+      dstPath: `users/${postAuthorId}/notifications/${docId}`,
+    };
+
+    logicResultDocs.push(postLogicResultDoc);
+    logicResultDocs.push(likeLogicResultDoc);
+    logicResultDocs.push(notificationLogicResultDoc);
+
+    return {
+      name: "onUnlikeLogic",
+      status: "finished",
+      documents: logicResultDocs,
+    };
+  },
+};
+
 
 // TODO write onUnlikeLogic
 //  decrement post likesCount
---
+++ functions/test/business-logics/like-logics.test.ts
@@ -0,0 +1,117 @@
+import {admin, db} from "emberflow/lib";
+import {PostView, UserView} from "../../src/types";
+import {initTestEmberflow} from "../init-test-emberflow";
+import {Action, EventContext, LogicResultDoc} from "emberflow/lib/types";
+import {DocumentData, DocumentReference} from "firebase-admin/firestore";
+import {
+  onUnlikeLogic,
+} from "../../src/business-logics/like-logics";
+
+initTestEmberflow();
+
+const userId = "userId";
+const user: UserView = {
+  "@id": userId,
+  "avatarUrl": `users/${userId}/profile-picture.jpeg`,
+  "firstName": "Sample",
+  "lastName": "User",
+};
+
+const postId = "postId";
+const post: PostView = {
+  "@id": postId,
+  "createdBy": user,
+  "createdAt": admin.firestore.Timestamp.now().toDate(),
+};
+
+describe("onUnlikeLogic", () => {
+  const docId = "likeId";
+  const eventContext: EventContext = {
+    uid: userId,
+    docPath: `posts/${postId}/likes/${docId}`,
+    docId: docId,
+  } as EventContext;
+  const action: Action = {
+    eventContext: eventContext,
+    actionType: "create",
+    modifiedFields: {},
+    user: user,
+    document: {},
+    status: "new",
+    timeCreated: admin.firestore.Timestamp.now(),
+  } as Action;
+
+
+  const expectedPostLogicResultDoc: LogicResultDoc = {
+    action: "merge",
+    dstPath: `posts/${postId}`,
+    instructions: {
+      likesCount: "--",
+    },
+  };
+
+
+  const expectedLikeLogicResultDoc: LogicResultDoc = {
+    action: "delete",
+    dstPath: `posts/${postId}/likes/${docId}`,
+  };
+
+  const expectedNotificationLogicResultDoc: LogicResultDoc = {
+    action: "delete",
+    dstPath: `users/${userId}/notifications/${docId}`,
+  };
+
+  beforeEach(() => {
+    jest.spyOn(db, "doc").mockImplementation((docPath) => {
+      const path = docPath.split("/");
+      const postDocId = path[1];
+
+      if (path.length < 4) {
+        return {
+          parent: {
+            parent: null,
+          },
+        } as unknown as DocumentReference<DocumentData>;
+      }
+
+      return {
+        parent: {
+          parent: {
+            path: `posts/${postId}`,
+            get: jest.fn().mockResolvedValue({
+              data: () => postDocId === postId ? post : null,
+            }),
+          },
+        },
+      } as unknown as DocumentReference<DocumentData>;
+    });
+  });
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("should return finished logic result with 3 documents", async () => {
+    const result = await onUnlikeLogic.logicFn(action);
+
+    expect(result.name).toEqual("onUnlikeLogic");
+    expect(result.status).toEqual("finished");
+    expect(result.documents.length).toBe(3);
+  });
+
+  it("should return finished logic result to decrement post likes document",
+    async () => {
+      const result = await onUnlikeLogic.logicFn(action);
+      expect(result.documents[0]).toStrictEqual(expectedPostLogicResultDoc);
+    });
+  it("should return finished logic result to delete the userView from post document",
+    async () => {
+      const result = await onUnlikeLogic.logicFn(action);
+      expect(result.documents[1]).toStrictEqual(expectedLikeLogicResultDoc);
+    });
+
+  it("should return finished logic result to delete the notification document",
+    async () => {
+      const result = await onUnlikeLogic.logicFn(action);
+      expect(result.documents[2]).toStrictEqual(expectedNotificationLogicResultDoc);
+    });
+});
