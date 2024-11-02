ade3951bde7f7fbfacfb442468e48b08c7cb730a
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-11-02 11:44:44 +0800

    implemented onShareLogic and unit tests

--- functions/src/business-logics/share-logics.ts
+++ functions/src/business-logics/share-logics.ts
@@ -1,4 +1,80 @@
+import {Action, LogicConfig, LogicResultDoc} from "emberflow/lib/types";
+import {Entity} from "../db-structure";
+import {db} from "emberflow/lib";
 // TODO write onShareLogic
 //  copy post to user's timeline
 //  increment post sharesCount
 //  create a notification for post's author
+
+
+export const onShareLogic: LogicConfig = {
+  name: "onShareLogic",
+  actionTypes: ["create"],
+  modifiedFields: ["all"],
+  entities: [Entity.Share],
+  logicFn: async (action: Action) => {
+    const {
+      eventContext: {
+        docPath,
+      },
+      user,
+    } = action;
+    const shareDocRef = db.doc(docPath);
+    const postDocRef = shareDocRef.parent.parent;
+
+    if (!postDocRef) {
+      return {
+        name: "onShareLogic",
+        status: "error",
+        message: "Post not found",
+        documents: [],
+      };
+    }
+
+    const postDoc = await postDocRef.get();
+    const post = postDoc.data();
+    if (post === undefined) {
+      return {
+        name: "onShareLogic",
+        status: "error",
+        message: "Post not found",
+        documents: [],
+      };
+    }
+
+    const {
+      "@id": postId,
+      createdBy: {
+        "@id": postAuthorId,
+      },
+    } = post;
+    const timelineLogicResultDoc: LogicResultDoc = {
+      action: "copy",
+      dstPath: `users/${user["@id"]}/timeline/${postId}`,
+      srcPath: docPath,
+    };
+    const postLogicResultDoc: LogicResultDoc = {
+      action: "create",
+      dstPath: docPath,
+      instructions: {
+        "sharesCount": "++",
+      },
+    };
+
+    const notificationLogicResultDoc: LogicResultDoc = {
+      action: "create",
+      dstPath: `users/${postAuthorId}/notifications/${postId}`,
+    };
+    const logicResultDocs: LogicResultDoc[] = [];
+    logicResultDocs.push(timelineLogicResultDoc);
+    logicResultDocs.push(postLogicResultDoc);
+    logicResultDocs.push(notificationLogicResultDoc);
+
+
+    return {
+      name: "onShareLogic",
+      status: "finished",
+      documents: logicResultDocs,
+    };
+  },
+};
---
+++ functions/test/business-logics/share-logics.test.ts
@@ -0,0 +1,87 @@
+import {initTestEmberflow} from "../init-test-emberflow";
+import {PostView, UserView} from "../../src/types";
+import {Action, EventContext} from "emberflow/lib/types";
+import {admin, db} from "emberflow/lib";
+import {firestore} from "firebase-admin";
+import {onShareLogic} from "../../src/business-logics/share-logics";
+
+initTestEmberflow();
+
+const userView: UserView = {
+  "@id": "user",
+  "firstName": "John",
+  "lastName": "Doe",
+};
+
+const post: PostView = {
+  "@id": "post",
+  "createdAt": expect.any(admin.firestore.Timestamp),
+  "createdBy": userView,
+};
+
+
+describe("onShareLogic", () => {
+  const shareId = "shareId";
+  const docPath = `posts/${post["@id"]}/shares/${userView["@id"]}`;
+  const eventContext: EventContext = {
+    docId: shareId,
+    docPath: docPath,
+    uid: userView["@id"],
+
+  } as EventContext;
+
+  const action: Action = {
+    actionType: "create",
+    document: {},
+    eventContext: eventContext,
+    modifiedFields: {},
+    status: "new",
+    timeCreated: expect.any(admin.firestore.Timestamp),
+    user: userView,
+  } as Action;
+
+  beforeEach(() => {
+    jest.spyOn(db, "doc").mockImplementation(() => {
+      return {
+        parent: {
+          parent: {
+            get: jest.fn().mockResolvedValue({
+              data: jest.fn().mockReturnValue(post),
+            }),
+          },
+        },
+      } as unknown as firestore.DocumentReference;
+    });
+  });
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+  it("should return 3 documents", async () => {
+    const result = await onShareLogic.logicFn(action);
+    expect(result.documents.length).toEqual(3);
+  });
+
+  it("should return 1 document with action copy", async () => {
+    const result = await onShareLogic.logicFn(action);
+    expect(result.documents[0].action).toEqual("copy");
+    expect(result.documents[0].dstPath)
+      .toEqual(`users/${userView["@id"]}/timeline/${post["@id"]}`);
+    expect(result.documents[0].srcPath).toEqual(docPath);
+  });
+
+  it("should return 1 document with action create", async () => {
+    const result = await onShareLogic.logicFn(action);
+    expect(result.documents[1].action).toEqual("create");
+    expect(result.documents[1].dstPath).toEqual(docPath);
+    expect(result.documents[1].instructions)
+      .toEqual({sharesCount: "++"});
+  });
+
+  it("should return 1 document with action create", async () => {
+    const result = await onShareLogic.logicFn(action);
+    expect(result.documents[2].action).toEqual("create");
+    expect(result.documents[2].dstPath)
+      .toEqual(`users/${post.createdBy["@id"]}/notifications/${post["@id"]}`);
+  });
+});
