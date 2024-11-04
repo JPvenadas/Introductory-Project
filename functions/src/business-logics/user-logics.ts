b42f82558991d92d15afe6dac441c07658b43f46
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-11-05 07:54:16 +0800

    initial changes

--- functions/src/business-logics/user-logics.ts
+++ functions/src/business-logics/user-logics.ts
@@ -2,3 +2,22 @@
 //  allow users to update their firstName, lastName, birthDate, and avatarUrl
 //  avatarUrl must be saved to users/{uid}/profile-pictures path in storage
 //  create a security fn that will prevent users from changing their username
+
+import {Entity} from "../db-structure";
+import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
+
+export const onUserUpdateLogic: LogicConfig = {
+  name: "onUserUpdateLogic",
+  actionTypes: ["update"],
+  entities: [Entity.User],
+  modifiedFields: ["all"],
+  logicFn: async (action) => {
+    const logicResultDocs: LogicResultDoc[] = [];
+    return {
+      name: "onUserUpdateLogic",
+      status: "finished",
+      documents: logicResultDocs,
+
+    };
+  },
+};
