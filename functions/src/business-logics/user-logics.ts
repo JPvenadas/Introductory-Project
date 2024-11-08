b7491eaf9eb32dd7b47cc0eaef0d864364135de2
Jeremy Jobert Peñafiel <jeremyjobert.penafiel@gmail.com>
2024-11-08 23:01:08 +0800

    created logics and userLogics test

--- functions/src/business-logics/user-logics.ts
+++ functions/src/business-logics/user-logics.ts
@@ -1,18 +1,71 @@
+import {Entity} from "../db-structure";
+import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
+import {deleteStorageFile, getFileDataFromUrl, moveStorageFile} from "./utils";
+import {DocumentData} from "firebase-admin/firestore";
+
 // TODO write onUserUpdateLogic
 //  allow users to update their firstName, lastName, birthDate, and avatarUrl
 //  avatarUrl must be saved to users/{uid}/profile-pictures path in storage
 //  create a security fn that will prevent users from changing their username
-
-import {Entity} from "../db-structure";
-import {LogicConfig, LogicResultDoc} from "emberflow/lib/types";
-
 export const onUserUpdateLogic: LogicConfig = {
   name: "onUserUpdateLogic",
   actionTypes: ["update"],
   entities: [Entity.User],
   modifiedFields: ["all"],
   logicFn: async (action) => {
+    const {
+      eventContext: {
+        docPath,
+        docId,
+        uid,
+      },
+      modifiedFields,
+      document: oldUser,
+    } = action;
+
+    const {
+      avatarUrl: fileUrl,
+      ...fields
+    } = modifiedFields;
+
+    const newUserDoc: DocumentData = {...fields};
+
+    if (fileUrl) {
+      const {
+        // filename: newFileName,
+        fileExtension: newFileExtension,
+      } = getFileDataFromUrl(fileUrl);
+      const {
+        avatarUrl: oldFileUrl,
+      } = oldUser;
+
+      if (oldFileUrl) {
+        const {
+          fileExtension: oldFileExtension,
+        }= getFileDataFromUrl(oldFileUrl);
+
+
+        if (newFileExtension !== oldFileExtension) {
+          await deleteStorageFile(oldFileUrl);
+        }
+      }
+      const fileDstPath =
+        `users/${uid}/profile-pictures/${docId}.${newFileExtension}`;
+      await moveStorageFile(fileUrl, fileDstPath);
+
+      // const {mimeType, size}= await getFileMetadata(fileUrl);
+      newUserDoc["avatarUrl"] = fileUrl;
+    }
+
+    const userLogicResultDoc: LogicResultDoc = {
+      action: "merge",
+      dstPath: docPath,
+      doc: newUserDoc,
+    };
+
     const logicResultDocs: LogicResultDoc[] = [];
+    logicResultDocs.push(userLogicResultDoc);
+
     return {
       name: "onUserUpdateLogic",
       status: "finished",
@@ -21,3 +74,7 @@ export const onUserUpdateLogic: LogicConfig = {
     };
   },
 };
+
+
+// reject logic if may suername
+// delete old avatar url
--- functions/src/security.ts
+++ functions/src/security.ts
@@ -1,5 +1,5 @@
 import {Entity} from "./db-structure";
-import {SecurityConfig, SecurityFn} from "emberflow/lib/types";
+import {SecurityConfig, SecurityFn, SecurityResult} from "emberflow/lib/types";
 
 // A security function that allows all actions
 const allAllowed: SecurityFn = async (
@@ -15,9 +15,35 @@ const allAllowed: SecurityFn = async (
   };
 };
 
+const userSecurityFn: SecurityFn = async (
+  actionType,
+  modifiedFields,
+) => {
+  const allowed: SecurityResult = {
+    status: "allowed",
+  };
+  const rejected: SecurityResult = {
+    status: "rejected",
+    // message: "You are not allowed to change your username",
+  };
+
+  if (actionType === "update") {
+    const fields: string[] = Object.keys(modifiedFields);
+
+    for (const field of fields) {
+      if (field === "username") {
+        rejected["message"] = "security.user.update.username.rejected";
+        return rejected;
+      }
+    }
+    return rejected;
+  }
+  return allowed;
+};
+
 export const securityConfig: SecurityConfig = {
   // Implement your security functions for each entity here
-  [Entity.User]: allAllowed,
+  [Entity.User]: userSecurityFn,
   [Entity.Notification]: allAllowed,
   [Entity.Post]: allAllowed,
   [Entity.Comment]: allAllowed,
